859b2eff5f8af7090837506c4c972026bf079b7c
Maksym Chernetskyi <konomerubokkusu@gmail.com>
2023-03-19 20:29:04 +0100

    levenstein distance updated

--- python/levensteinD.py
+++ python/levensteinD.py
@@ -2,21 +2,19 @@ string1 = "ATTGCACGACATAT"
 string2 = "ATTCGACGACGTACC"
 
 
-#recursive approach
-
-def D(str1, str2):
+def levenstein_distance(str1, str2):
     if str1 == '' and str2 == '':
         return 0
     elif str1 == '' or str2 == '':
         return len(str1 or str2)
 
     if str1[-1:] == str2[-1:]:
-        return D(str1[:-1],str2[:-1])
+        return levenstein_distance(str1[:-1],str2[:-1])
     else:
-        m = min( D(str1,str2[:-1]),D(str1[:-1],str2),D(str1[:-1],str2[:-1]) )
+        m = min( levenstein_distance(str1,str2[:-1]),levenstein_distance(str1[:-1],str2),levenstein_distance(str1[:-1],str2[:-1]) )
         if str1[:-2] and str2[:-2]:
-            m = min(m,D(str1[:-2],str2[:-2]))
+            m = min(m,levenstein_distance(str1[:-2],str2[:-2]))
         return m+1
 
 if __name__ == "__main__":
-    print(100 - (D(string1,string2)/len(string1))*100, '%')
+    print(100 - (levenstein_distance(string1,string2)/len(string1))*100, '%')
